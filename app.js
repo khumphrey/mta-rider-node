6a24b711e7b34002b6973a96d06a9a9c0a370b90
khumphrey <khumphrey@mdsol.com>
2020-04-10 11:26:31 -0400

    fix error handling

--- app.js
+++ app.js
@@ -15,12 +15,14 @@ app.get('/stops', (req, res, next) => {
 
   mta.stop(query)
     .then(result => res.send(result))
+    .catch(err => next(err))
 })
 
 // example query domain/stops/110
 app.get('/stops/:id', (req, res, next) => {
   mta.stop(req.params.id)
     .then(result => res.send(result))
+    .catch(err => next(err))
 })
 
 // example query domain/status?type=MetroNorth
@@ -30,11 +32,12 @@ app.get('/status', (req, res, next) => {
   if (req.query.type && !allowedTypes.includes(req.query.type)) {
     const error = new Error('Your request was not processable because the parameters are not recognizable')
     error.status = 422
-    throw error
+    return next(error)
   }
 
   mta.status(req.query.type)
     .then(result => res.send(result))
+    .catch(err => next(err))
 })
 
 // example query domain/schedule?stopIds=[635,110]&feedId=1
@@ -43,11 +46,12 @@ app.get('/schedule', (req, res, next) => {
   if (!req.query.stopIds) {
     const error = new Error('Your request was not processable because the parameters are not recognizable')
     error.status = 422
-    throw error
+    return next(error)
   }
 
   mta.schedule(JSON.parse(req.query.stopIds), req.query.feedId)
     .then(result => res.send(result))
+    .catch(err => next(err))
 })
 
 // for everything that doesn't match above
